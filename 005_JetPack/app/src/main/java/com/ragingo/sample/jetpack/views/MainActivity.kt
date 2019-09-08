e81ac2feb5ca2f65a552d2323b0d015e6aaa101d
ragingo <save.the.world.crush@gmail.com>
2019-09-09 00:51:03 +0900

    Observer

--- 005_JetPack/app/src/main/java/com/ragingo/sample/jetpack/views/MainActivity.kt
+++ 005_JetPack/app/src/main/java/com/ragingo/sample/jetpack/views/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.ragingo.sample.jetpack.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.ragingo.sample.jetpack.R
+import com.ragingo.sample.jetpack.data.UserInfo
 import com.ragingo.sample.jetpack.databinding.ActivityMainBinding
 import com.ragingo.sample.jetpack.viewmodels.main.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = MainActivity::class.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +23,18 @@ class MainActivity : AppCompatActivity() {
         val bindings: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        val user1 = UserInfo()
+        user1.id = "u001"
+        user1.name = "user 1"
+        user1.age = 20
+
         val vm = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        vm.userInfo.value!!.id = "u001"
-        vm.userInfo.value!!.name = "user 1"
-        vm.userInfo.value!!.age = 20
+        vm.userInfo.value!!.id = user1.id
+        vm.userInfo.value!!.name = user1.name
+        vm.userInfo.value!!.age = user1.age
+        vm.userInfo.observe(this, Observer {
+            Log.d(TAG, "${it.id}, ${it.name}, ${it.age}")
+        })
 
         bindings.vm = vm
         bindings.lifecycleOwner = this
