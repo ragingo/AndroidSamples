2f2a69ba70ec80c881e5f62c558c7debcd3031d1
ragingo <save.the.world.crush@gmail.com>
2020-05-03 22:31:22 +0900

    [006_NavigationView] ライブラリ更新、コード整理

--- 006_NavigationView/app/src/main/java/com/ragingo/sample/navigationview/MainActivity.kt
+++ 006_NavigationView/app/src/main/java/com/ragingo/sample/navigationview/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.ragingo.sample.navigationview
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import android.view.Menu
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
